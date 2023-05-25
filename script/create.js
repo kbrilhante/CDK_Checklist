c118432ce650ab93e757a844499c2d9c27f47646
Karina <kbrilhante@gmail.com>
2023-05-25 11:34:49 -0300

    Initial commit

---
+++ script/create.js
@@ -0,0 +1,17 @@
+initialize();
+
+function initialize() {
+    const list = localStorage.getItem("list");
+    if (list) {
+        document.querySelector("textarea").value = list;
+    }
+}
+
+function create() {
+    const list = document.querySelector("textarea").value;
+    if (list) {
+        console.log(list);
+        localStorage.setItem("list", list);
+        location = "checklist.html";
+    }
+}

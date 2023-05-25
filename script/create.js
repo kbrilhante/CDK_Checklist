9d74026a8f2938575645e878c202e537273b1e7e
Karina <kbrilhante@gmail.com>
2023-05-25 13:13:51 -0300

    Update create.js

--- script/create.js
+++ script/create.js
@@ -1,10 +1,15 @@
 initialize();
 
+
 function initialize() {
     const list = localStorage.getItem("list");
     if (list) {
         document.querySelector("textarea").value = list;
     }
+    document.querySelectorAll("button").forEach((button) => {
+        button.addEventListener("mouseenter", mouseOverButton);
+        button.addEventListener("mouseleave", mouseLeaveButton);
+    });
 }
 
 function create() {
@@ -15,3 +20,13 @@ function create() {
         location = "checklist.html";
     }
 }
+
+function del() {
+    const list = document.querySelector("textarea");
+    list.value = "";
+    localStorage.removeItem("list");
+}
+
+function mouseOverButton(e) {}
+
+function mouseLeaveButton(e) {}
